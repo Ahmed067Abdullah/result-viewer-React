8f8cce25cdd23eac005584bd14e3e527d2a59f73
Ahmed067Abdullah <ahmed@sudofy.com>
2020-07-17 13:50:16 +0500

    average CGPA stats added

--- src/components/facts-modal/FactsModal.js
+++ src/components/facts-modal/FactsModal.js
@@ -12,9 +12,14 @@ const FactsModal = ({ data, open, handleClose }) => {
   let studentsWithAbove3point7CGPA = 0;
   let studentsWithAbove3point5CGPA = 0;
   let studentsWithAbove3CGPA = 0;
+  let totalCGPAs = 0;
+  let totalCGPAOfBoys = 0;
+  let totalCGPAOfGirls = 0;
+  let totalBoys = 0;
+  let totalGirls = 0;
 
   for (let i = 0; i < data.length; i++) {
-    const { results, name, cgpa } = data[i];
+    const { results, name, cgpa, gender } = data[i];
 
     let count4GPAs = 0;
     for (let j = 0; j < results.length; j++) {
@@ -38,6 +43,14 @@ const FactsModal = ({ data, open, handleClose }) => {
         }
       }
     }
+    totalCGPAs += +cgpa;
+    if (gender) {
+      totalCGPAOfBoys += +cgpa;
+      totalBoys++;
+    } else {
+      totalCGPAOfGirls += +cgpa;
+      totalGirls++;
+    }
   }
 
 
@@ -51,6 +64,10 @@ const FactsModal = ({ data, open, handleClose }) => {
     }
   }
 
+  const avgCGPA = (totalCGPAs / data.length).toFixed(3);
+  const avgCGPAOfBoys = (totalCGPAOfBoys / totalBoys).toFixed(3);
+  const avgCGPAOfGirls = (totalCGPAOfGirls / totalGirls).toFixed(3);
+
   return (
     <ReactModal
       isOpen={open}
@@ -62,13 +79,18 @@ const FactsModal = ({ data, open, handleClose }) => {
       </div>
       <div className={classes['content-container']}>
         <ol>
-        <li>Total <span>{studentsScoring4GPAs}</span> students have secured 4 GPA in a semester till now.</li>
-        <li><span>{max4GPAsByStudent.name}</span> has scored 4 GPA the most <span>({max4GPAsByStudent.count})</span> times.</li>
-        <li>Semester <span>{max4GPAsInOneSemester.number}</span> had the most <span>({max4GPAsInOneSemester.count})</span> 4 GPA scoring students.</li>
-        <li>Semester <span>{semestersWithNo4GPAs.join(",")}</span> had no student able to score 4 GPA.</li>
-        <li>There are <span>{studentsWithAbove3point7CGPA}</span> students having CGPA over 3.7.</li>
-        <li>There are <span>{studentsWithAbove3point5CGPA}</span> students having CGPA over 3.5.</li>
-        <li>There are <span>{studentsWithAbove3CGPA}</span> students having CGPA over 3.0.</li>
+          <li>Total <span>{studentsScoring4GPAs}</span> students have secured 4 GPA in a semester till now.</li>
+          <li><span>{max4GPAsByStudent.name}</span> has scored 4 GPA the most <span>({max4GPAsByStudent.count})</span> times.</li>
+          <li>Semester <span>{max4GPAsInOneSemester.number}</span> had the most <span>({max4GPAsInOneSemester.count})</span> 4 GPA scoring students.</li>
+          <li>Semester <span>{semestersWithNo4GPAs.join(",")}</span> had no student able to score 4 GPA.</li>
+          <li>None of the <span>boys</span> have ever scored 4 GPA</li>
+          <li>The average CGPA of the class is <span>{avgCGPA}</span></li>
+          <li>The average CGPA of Boys in the class is <span>{avgCGPAOfBoys}</span></li>
+          <li>The average CGPA of Girls in the class is <span>{avgCGPAOfGirls}</span></li>
+          <li>There are <span>{studentsWithAbove3point7CGPA}</span> students having CGPA over 3.7.</li>
+          <li>There are <span>{studentsWithAbove3point5CGPA}</span> students having CGPA over 3.5.</li>
+          <li>There are <span>{studentsWithAbove3CGPA}</span> students having CGPA over 3.0.</li>
+          <li>Aap stable hote reh jaenge, unk 2 bche bi ho chuke hnge <span role="img" aria-label="">😂😂</span>.</li>
         </ol>
       </div>
     </ReactModal >
